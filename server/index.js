7088d58289cc266b5fbbba15884eb3b93963edc0
Prasanna Kumar <kumar22maran@gmail.com>
2024-06-10 11:47:01 +0530

    modified index to facilitate db connection

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const express=require('express')
 const app=express()
 
 const mongoose=require('mongoose')
+const {connectDatabase} = require("./Database/DBConnect");
+
+
 
 require('dotenv').config()
 
@@ -11,7 +14,12 @@ app.use(express.json())
 
 
 
+
+const uri=process.env.MONGO_URI
+// console.log(uri)
 const port=process.env.PORT
+
+connectDatabase(uri)
 
 app.listen(port,()=>{
     console.debug(`Server started on port ${port}`)
